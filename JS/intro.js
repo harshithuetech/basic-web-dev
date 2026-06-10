8559e09257ce3dab3af58e188ab367ef422103e0
harshithuetech <harshitraul@udyogsuvidhakendra.in>
2026-06-10 14:40:49 +0530

    JS Added

---
+++ JS/intro.js
@@ -0,0 +1,123 @@
+// console.log("external Js");
+
+// let 
+
+// let name = "harshit";
+// console.log(name);
+// let name = "Test";
+// console.log(name);
+
+
+// var
+// var name = "Harshit";
+// console.log(name);
+// var name = "Test";
+// console.log(name);
+
+
+// const
+// const name = "Harshit";
+// name = "Test";
+// console.log(name);
+
+
+// Data Types
+// String
+var city = "mumbai";
+//  var city = 'mumbai';
+//  var city = `mumbai`;
+
+// console.log("My City Name Is " + city)
+// console.log(`My City Name Is ${city}`)
+
+// Number
+//  var count = 15;
+var count = 1.3455;
+// console.log(typeof count);
+
+// Boolean 
+var isCorrect = true;
+var isCorrect = false;
+
+// console.log(typeof isCorrect);
+
+
+// var test;
+// console.log(test)
+// console.log(typeof test)
+
+// var nullValue = null;
+// console.log(nullValue)
+// console.log(typeof nullValue);
+
+
+// Operators 
+// operand1 operator  operand2
+
+// Types Of Operator
+// Arithmetic
+var num1 = 10;
+var num2 = 2;
+// var sum = num1 + num2;
+// var sub = num1 - num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
+// var mod = num1 % num2;
+var expo = num1 ** num2;
+
+// var sum = 10 + 5;
+
+
+// console.log(sum);
+// console.log(typeof num1)
+// console.log(typeof num2)
+// console.log(typeof sum)
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+// console.log(mod);
+// console.log(expo);
+
+// console.log(num1++);
+// var incVal = num1++;
+// console.log(incVal)
+
+
+// Comparison
+var num1 = 10;
+var num2 = "10";
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
+
+
+// logical
+// AND -> &&
+// OR -> ||
+// NOT -> !
+
+// console.log(true && false)
+// console.log(true || false)
+// console.log(!false)
+
+// Assignment
+// =
+
+// var num = 5;
+
+// num += 7;
+// num = num + 7;
+// console.log(num);
+
+// ternary
+var age = 18;
+var haveLicense = false
+var result = (age >= 18 && haveLicense) ? "Pass" : "Fail";
+console.log(result);
+
